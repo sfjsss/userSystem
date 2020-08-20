73f3f1cdb0d8e7c4aed8c000232a5b900e892f9e
Tianyu Li <alandron06281990@gmail.com>
2020-08-19 23:36:56 -0700

    done

---
+++ src/main/java/com/tianyuli/usersystem/rpcDomain/req/UserProfileReq.java
@@ -0,0 +1,94 @@
+package com.tianyuli.usersystem.rpcDomain.req;
+
+import java.io.Serializable;
+
+public class UserProfileReq implements Serializable {
+
+    private static final long serialVersionUID = 4912067477954015951L;
+
+    private String username;
+    private String email;
+    private String personalProfile;
+    private String country;
+    private String province;
+    private String city;
+    private String streetAddress;
+    private String areaNumber;
+    private String phoneNumber;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPersonalProfile() {
+        return personalProfile;
+    }
+
+    public void setPersonalProfile(String personalProfile) {
+        this.personalProfile = personalProfile;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getAreaNumber() {
+        return areaNumber;
+    }
+
+    public void setAreaNumber(String areaNumber) {
+        this.areaNumber = areaNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- src/main/java/com/tianyuli/usersystem/service/UserProfileServiceImpl.java
+++ src/main/java/com/tianyuli/usersystem/service/UserProfileServiceImpl.java
@@ -1,10 +1,20 @@
 package com.tianyuli.usersystem.service;
 
 import com.tianyuli.usersystem.dao.UserProfileDao;
+import com.tianyuli.usersystem.pojo.Address;
+import com.tianyuli.usersystem.pojo.User;
+import com.tianyuli.usersystem.pojo.UserPreference;
 import com.tianyuli.usersystem.pojo.UserProfile;
+import com.tianyuli.usersystem.rpcDomain.common.RespResult;
+import com.tianyuli.usersystem.rpcDomain.common.ResultCode;
+import com.tianyuli.usersystem.rpcDomain.req.UserProfileReq;
+import com.tianyuli.usersystem.rpcDomain.resp.UserProfileResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserProfileServiceImpl extends BaseServiceImpl<UserProfile, String> implements UserProfileService {
@@ -12,8 +22,61 @@ public class UserProfileServiceImpl extends BaseServiceImpl<UserProfile, String>
     @Autowired
     private UserProfileDao userProfileDao;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AddressService addressService;
+
+    @Autowired
+    private UserPreferenceService userPreferenceService;
+
     @Override
     protected JpaRepository getJpaRepository() {
         return this.userProfileDao;
+    }
+
+    @Override
+    public RespResult updateProfileByUserId(String userIdByAuthorHead, UserProfileReq userProfileReq) {
+        Optional<User> user = userService.findById(userIdByAuthorHead);
+        if (!user.isPresent()) {
+            return new RespResult(ResultCode.USER_NOT_EXIST);
+        }
+        User foundUser = user.get();
+        Address address = addressService.findById(foundUser.getId()).get();
+        UserPreference userPreference = userPreferenceService.findById(foundUser.getId()).get();
+        UserProfile userProfile = userProfileDao.findById(foundUser.getId()).get();
+
+        BeanUtils.copyProperties(userProfileReq, foundUser);
+        BeanUtils.copyProperties(userProfileReq, address);
+        BeanUtils.copyProperties(userProfileReq, userPreference);
+        BeanUtils.copyProperties(userProfileReq, userProfile);
+
+        userService.save(foundUser);
+        addressService.save(address);
+        userPreferenceService.save(userPreference);
+        userProfileDao.save(userProfile);
+
+        return new RespResult(ResultCode.SUCCESS);
+    }
+
+    @Override
+    public RespResult getUserProfileById(String userIdByAuthorHead) {
+        Optional<User> user = userService.findById(userIdByAuthorHead);
+        if (!user.isPresent()) {
+            return new RespResult(ResultCode.USER_NOT_EXIST);
+        }
+        User foundUser = user.get();
+        Address address = addressService.findById(foundUser.getId()).get();
+        UserPreference userPreference = userPreferenceService.findById(foundUser.getId()).get();
+        UserProfile userProfile = userProfileDao.findById(foundUser.getId()).get();
+        UserProfileResp userProfileResp = new UserProfileResp();
+
+        BeanUtils.copyProperties(foundUser, userProfileResp);
+        BeanUtils.copyProperties(address, userProfileResp);
+        BeanUtils.copyProperties(userPreference, userProfileResp);
+        BeanUtils.copyProperties(userProfile, userProfileResp);
+
+        return new RespResult(ResultCode.SUCCESS, userProfileResp);
     }
 }
